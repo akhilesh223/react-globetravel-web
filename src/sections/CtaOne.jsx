51ada8f2eb4c6a31cf5521ceacc8920be00bfb93
Akhilesh223 <akhileshnishad287@gmail.com>
2026-05-24 16:39:07 -0800

    add the best images in the webside

--- src/sections/CtaOne.jsx
+++ src/sections/CtaOne.jsx
@@ -1,6 +1,6 @@
 // import React from 'react'
 import { useEffect } from 'react'
-import cta1 from '../assets/cta1.webp'
+import cta1 from '../assets/cta1.png'
 import{FaPlay} from 'react-icons/fa'
 import AOS from 'aos';
 import 'aos/dist/aos.css'
@@ -26,9 +26,9 @@ function CtaOne() {
       {/* content part */}
       <div className='relative z-10 flex flex-col justify-center items-center gap-5'>
         <h1 data-aos ='zoom-in' data-aos-delay='100' className=' lg:text-5xl text-3xl
-        font-fakhwan font-semibold text-white lg:w-[60%] w-full text-center capitalize'>Lorem ipsum dolor sit amet.</h1>
-        <p data-aos ='zoom-in' data-aos-delay='200' className='text-white lg:w-[50%] w-full text-lg text-center'>Lorem ipsum dolor sit amet 
-          consectetur adipisicing elit. Adipisci vero molestiae quae harum fugiat vitae, quis vel deserunt pariatur numquam!</p>
+        font-fakhwan font-semibold text-white lg:w-[60%] w-full text-center capitalize'>Experience breathtaking destinations, unforgettable adventures, and luxurious journeys crafted to inspire your soul.....</h1>
+        <p data-aos ='zoom-in' data-aos-delay='200' className='text-white lg:w-[50%] w-full text-lg text-center'>
+          Discover hidden wonders, luxury escapes, and adventures that inspire your soul.</p>
            <div className="w-12 h-12 lg:w-20 lg:h-20 bg-white rounded-full flex items-center justify-center">
          <FaPlay className="text-orange-600 text-xl lg:text-3xl" />
 </div>
